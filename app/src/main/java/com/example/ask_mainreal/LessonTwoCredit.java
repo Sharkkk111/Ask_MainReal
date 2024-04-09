50d8f5f617be13c11683ba02b849a7d6eb3372f6
keirareid <keirareid20@gmail.com>
2024-04-09 13:42:54 -0400

    main2

---
+++ app/src/main/java/com/example/ask_mainreal/LessonTwoCredit.java
@@ -0,0 +1,12 @@
+package com.example.ask_mainreal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LessonTwoCredit extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson1_1_2);}
+}
