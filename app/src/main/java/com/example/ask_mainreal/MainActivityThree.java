a3911b1e2d4855305434a8990f9aa6831c6192a2
keirareid <keirareid20@gmail.com>
2024-04-09 13:42:54 -0400

    main2

--- app/src/main/java/com/example/ask_mainreal/MainActivityThree.java
+++ app/src/main/java/com/example/ask_mainreal/MainActivityThree.java
@@ -1,12 +1,35 @@
 package com.example.ask_mainreal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivityThree extends AppCompatActivity {
+    Button credit1;
+    Button credit2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);}
-}
+        setContentView(R.layout.activity_main2);
+
+    credit1=findViewById(R.id.credit1);
+        credit1.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            Intent intent = new Intent(MainActivityThree.this,LessonOneCredit.class);
+            startActivity(intent);
+        }
+    });
+        credit2=findViewById(R.id.credit2);
+        credit2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivityThree.this,LessonOneCredit.class);
+                startActivity(intent);
+            }
+        });
+
+}}
