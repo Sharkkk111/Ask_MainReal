1be6fe70ef605694a000dad9b0a3d42b09efb4b4
shauryapatni <shauryapatni11@gmail.com>
2024-04-11 23:29:27 -0400

    Changed images

--- app/src/main/java/com/example/ask_mainreal/lesson_1.java
+++ app/src/main/java/com/example/ask_mainreal/lesson_1.java
@@ -5,12 +5,6 @@ import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.widget.MediaController;
-import android.widget.VideoView;
-
 public class lesson_1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +15,4 @@ public class lesson_1 extends AppCompatActivity {
         videoView.setVideoPath();
         videoView.start();
 
-        VideoView videoView = (VideoView) findViewById(R.id.videoView);  //casting to VideoView is not Strictly required above API level 26
-        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.how_do_credit_cards_work); //set the path of the video that we need to use in our VideoView
-        videoView.start();  //start() method of the VideoView class will start the video to play
-        MediaController mediaController = new MediaController(this);
-//link mediaController to videoView
-        mediaController.setAnchorView(videoView);
-//allow mediaController to control our videoView
-        videoView.setMediaController(mediaController);
-        setContentView(R.layout.lesson_1);
-    }
 }
