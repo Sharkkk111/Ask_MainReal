c71b53b2fec3e1515e39e248c4d14739bc8fd04d
keirareid <keirareid20@gmail.com>
2024-04-12 11:02:56 -0400

    buttons

--- app/src/main/java/com/example/ask_mainreal/QuizOneCredit.java
+++ app/src/main/java/com/example/ask_mainreal/QuizOneCredit.java
@@ -7,6 +7,6 @@ public class QuizOneCredit extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);}
+        setContentView(R.layout.lesson_1);}
 
 }
