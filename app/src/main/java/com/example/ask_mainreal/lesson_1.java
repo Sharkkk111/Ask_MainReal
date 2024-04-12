cb94657af36c1211a55b7ca39954e8ae0b48b086
shauryapatni <shauryapatni11@gmail.com>
2024-04-11 23:29:27 -0400

    Changed images

---
+++ app/src/main/java/com/example/ask_mainreal/lesson_1.java
@@ -0,0 +1,18 @@
+package com.example.ask_mainreal;
+
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class lesson_1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson_1);}
+
+        final VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath();
+        videoView.start();
+
+}
