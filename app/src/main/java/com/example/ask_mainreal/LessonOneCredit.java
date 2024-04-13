e4d8d8ef4de2cff93564c0c665e70ef0e3a5aa70
keirareid <keirareid20@gmail.com>
2024-04-12 21:55:52 -0400

    buttons

--- app/src/main/java/com/example/ask_mainreal/LessonOneCredit.java
+++ app/src/main/java/com/example/ask_mainreal/LessonOneCredit.java
@@ -1,37 +1,14 @@
 package com.example.ask_mainreal;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LessonOneCredit extends AppCompatActivity {
-
-    Button home;
-    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lesson_1);
-
-        home = findViewById(R.id.home);
-        home.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(LessonOneCredit.this, HomePage.class );
-                startActivity(intent);
-            }
-        });
-
-        back = findViewById(R.id.back_button);
-        back.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(LessonOneCredit.this,MainActivityThree.class );
-                startActivity(intent);
-            }
-        });
-    }
+        setContentView(R.layout.lesson_1);}
 }
