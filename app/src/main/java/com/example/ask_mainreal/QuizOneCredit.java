8abbda64272ddadcf28d76f51fca97786a2ea0ad
keirareid <keirareid20@gmail.com>
2024-04-12 21:12:26 -0400

    buttons

--- app/src/main/java/com/example/ask_mainreal/QuizOneCredit.java
+++ app/src/main/java/com/example/ask_mainreal/QuizOneCredit.java
@@ -10,7 +10,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class QuizOneCredit extends AppCompatActivity{
-    Button choice;
+    Button a;
+    Button b;
+    Button c;
+    Button d;
     Button submit;
     EditText ShortAnswer;
     SharedPreferences sp;
@@ -23,20 +26,58 @@ public class QuizOneCredit extends AppCompatActivity{
     ShortAnswer = findViewById(R.id.editText);
     submit = findViewById(R.id.enter);
 
-    sp = getSharedPreferences("MyUserPreferences", Context.MODE_PRIVATE);
+    sp = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
         submit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 ShortAnswerStr = ShortAnswer.getText().toString();
 
                 SharedPreferences.Editor editor = sp.edit();
+                if(ShortAnswerStr.equals("correct answer")){
+                    editor.putBoolean("answer1", true);}
 
-                editor.putString("answer", ShortAnswerStr);
                 editor.apply();
-
-
             }
         });
-
+        a = findViewById(R.id.a);
+        a.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(QuizOneCredit.this, LessonOneCredit.class );
+                startActivity(intent);
+            }
+        });
+        c = findViewById(R.id.c);
+        c.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(QuizOneCredit.this, LessonOneCredit.class );
+                startActivity(intent);
+            }
+        });
+        d = findViewById(R.id.d);
+        d.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(QuizOneCredit.this, LessonOneCredit.class );
+                startActivity(intent);
+            }
+        });
+        b = findViewById(R.id.b);
+        b.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                SharedPreferences.Editor editor = sp.edit();
+                    editor.putBoolean("answer2", true);
+                editor.apply();
+            }
+        });
+        if(sp.getBoolean("answer1",false)&&sp.getBoolean("answer2",false)){
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putInt("creditLesson", 1);
+            editor.apply();
+            Intent intent = new Intent(QuizOneCredit.this, LessonComplete.class );
+            startActivity(intent);
+        }
     }
 }
