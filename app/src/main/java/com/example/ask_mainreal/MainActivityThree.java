ceffd77b0c70a1b1453140709085c7c91d11cec5
keirareid <keirareid20@gmail.com>
2024-04-12 21:49:38 -0400

    buttons

--- app/src/main/java/com/example/ask_mainreal/MainActivityThree.java
+++ app/src/main/java/com/example/ask_mainreal/MainActivityThree.java
@@ -1,6 +1,8 @@
 package com.example.ask_mainreal;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,20 +18,31 @@ public class MainActivityThree extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-    credit1=findViewById(R.id.credit1);
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+
+
+        credit1=findViewById(R.id.credit1);
         credit1.setOnClickListener(new View.OnClickListener(){
         @Override
         public void onClick(View view){
-            Intent intent = new Intent(MainActivityThree.this,LessonOneCredit.class);
-            startActivity(intent);
+            if(sp.getInt("credit",0) ==0){
+                Intent intent = new Intent(MainActivityThree.this,LessonOneCredit.class);
+                startActivity(intent);}
+            if(sp.getInt("credit",0)==1){
+                Intent intent = new Intent(MainActivityThree.this,LessonTwoCredit.class);
+                startActivity(intent);}
         }
     });
         credit2=findViewById(R.id.credit2);
         credit2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(MainActivityThree.this,LessonOneCredit.class);
-                startActivity(intent);
+                if(sp.getInt("credit",0) ==0){
+                    Intent intent = new Intent(MainActivityThree.this,LessonOneCredit.class);
+                    startActivity(intent);}
+                if(sp.getInt("credit",0)==1){
+                    Intent intent = new Intent(MainActivityThree.this,LessonTwoCredit.class);
+                    startActivity(intent);}
             }
         });
 
