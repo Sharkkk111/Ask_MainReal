48f2641aeda11e961d2a625edd23c34da4b947d7
keirareid <keirareid20@gmail.com>
2024-04-12 20:37:36 -0400

    buttons

--- app/src/main/java/com/example/ask_mainreal/QuizOneCredit.java
+++ app/src/main/java/com/example/ask_mainreal/QuizOneCredit.java
@@ -1,12 +1,42 @@
 package com.example.ask_mainreal;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class QuizOneCredit extends AppCompatActivity{
+    Button choice;
+    Button submit;
+    EditText ShortAnswer;
+    SharedPreferences sp;
+    String ShortAnswerStr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lesson_1);}
+        setContentView(R.layout.quiz1_1_1);
 
+    ShortAnswer = findViewById(R.id.editText);
+    submit = findViewById(R.id.enter);
+
+    sp = getSharedPreferences("MyUserPreferences", Context.MODE_PRIVATE);
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                ShortAnswerStr = ShortAnswer.getText().toString();
+
+                SharedPreferences.Editor editor = sp.edit();
+
+                editor.putString("answer", ShortAnswerStr);
+                editor.apply();
+
+
+            }
+        });
+
+    }
 }
