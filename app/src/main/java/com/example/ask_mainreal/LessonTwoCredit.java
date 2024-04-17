335fa0e34faca56b8e794ba6568292c763dd32ab
shauryapatni <shauryapatni11@gmail.com>
2024-04-17 10:49:15 -0400

    Changed images

--- app/src/main/java/com/example/ask_mainreal/LessonTwoCredit.java
+++ app/src/main/java/com/example/ask_mainreal/LessonTwoCredit.java
@@ -2,8 +2,10 @@ package com.example.ask_mainreal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,7 +17,11 @@ public class LessonTwoCredit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lesson1_1_2);
-
+        Log.d("Hi", "setContentView(R.layout.lesson_1)");
+        VideoView videoView = (VideoView) findViewById(R.id.videoView1_1_2);  //casting to VideoView is not Strictly required above API level 26
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.how_do_credit_cards_work); //set the path of the video that we need to use in our VideoView
+        videoView.start();  //start() method of the VideoView class will start the video to play
+        Log.d("Shaurya", "android.resource:// + getPackageName() + R.raw.how_do_credit_cards_work");
         home = findViewById(R.id.home);
         home.setOnClickListener(new View.OnClickListener(){
             @Override
