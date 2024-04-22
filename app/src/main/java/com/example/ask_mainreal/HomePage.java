f4549ee826f41878facfadb1e7dc2ff7814a8c04
keirareid <keirareid20@gmail.com>
2024-04-22 09:28:55 -0400

    updating current lesson

--- app/src/main/java/com/example/ask_mainreal/HomePage.java
+++ app/src/main/java/com/example/ask_mainreal/HomePage.java
@@ -1,6 +1,8 @@
 package com.example.ask_mainreal;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,18 +11,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePage extends AppCompatActivity {
 
-    Button credit1;
+    Button credit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
 
-    credit1=findViewById(R.id.credit1);
-        credit1.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View view){
-            Intent intent = new Intent(HomePage.this,LessonOneCredit.class);
-            startActivity(intent);
-        }
-    });
-}}
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+
+        credit=findViewById(R.id.credit1);
+        credit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int lesson = sp.getInt("creditLesson", 0);
+                if(lesson ==0){
+                    Intent intent = new Intent(HomePage.this,LessonOneCredit.class);
+                    startActivity(intent);}
+                else if(lesson==1){
+                    Intent intent = new Intent(HomePage.this,LessonTwoCredit.class);
+                    startActivity(intent);}
+            }
+
+        });}
+}
