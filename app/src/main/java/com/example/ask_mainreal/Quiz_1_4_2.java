381247b672468fe5a4a97426c72b54398c2afc48
keirareid <keirareid20@gmail.com>
2024-04-24 09:37:45 -0400

    current furthest version

---
+++ app/src/main/java/com/example/ask_mainreal/Quiz_1_4_2.java
@@ -0,0 +1,26 @@
+package com.example.ask_mainreal;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Quiz_1_4_2 extends AppCompatActivity {
+
+    Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz1_4_2);
+        button1 = findViewById(R.id.home);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Quiz_1_4_2.this, HomePage.class );
+                startActivity(intent);
+            }
+        });
+    }
+}
