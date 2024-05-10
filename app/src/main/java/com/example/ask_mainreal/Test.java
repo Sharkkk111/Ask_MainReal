e5e8fed2f07a7355519111dd68eb3f1f8639bb74
keirareid <keirareid20@gmail.com>
2024-05-09 21:45:36 -0400

    credit score

--- app/src/main/java/com/example/ask_mainreal/Test.java
+++ app/src/main/java/com/example/ask_mainreal/Test.java
@@ -1,6 +1,7 @@
 package com.example.ask_mainreal;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
@@ -11,8 +12,11 @@ public class Test extends AppCompatActivity {
 
     Button button1;
     Button button2;
+    SharedPreferences sp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sp.Editor.clear();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
